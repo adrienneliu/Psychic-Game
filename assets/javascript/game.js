ebb7238ad7a30fec0b233e818d2468c2d80480a3
Adrienne <iliu.adrienne@gmail.com>
2019-05-23 13:16:05 -0700

    add things I want to add into the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// set the counters to 0.
+
+var winCount = 0; 
+var loseCount = 0; 
+var guessesCount = 9; 
+var guessChoice = []; 
+
+// all the letters that the computer can guess 
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+
+
+// make the computer guess a letter
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+// show user win
+
+// shower user loss
+
+// show amount of guesses left
+
+// show user what they've guessed so far
+
+
+// reset the win and loss count after guesscount reaches 0 (without refreshing the page)
