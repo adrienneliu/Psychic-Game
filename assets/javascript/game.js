ed4dbee48317937e358148f3412c1bcfa643c514
Adrienne <iliu.adrienne@gmail.com>
2019-05-23 21:12:06 -0700

    lose counter and reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,47 +6,51 @@ var guessLeft = 9;
 var guessMade = []; 
 var computerGuess = [];
 
-// all the letters that the computer can guess 
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+// all the letters that the computer can guess
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var computerGuess;
+    
 
+    // make the computer guess a letter
+function guessNewLetter(){
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // console.log(computerGuess);
+}     
 
-// make the computer guess a letter
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerGuess);
+function reset(){
+    guessMade=[];
+    document.getElementById("guess-made").textContent = guessMade;
+    guessLeft = 9;
+}
 
 
 // show user what they've guessed so far
 
 document.onkeyup = function(input){
 
-var userPressed = event.key.toLowerCase();
+    var userPressed = event.key.toLowerCase();
 
-guessMade.push(userPressed);
-document.getElementById("guess-made").textContent = guessMade;
+    guessMade.push(userPressed);
+    document.getElementById("guess-made").textContent = guessMade;
 
 
-//compare computer pick + user pick 
-// console.log(guessMade); 
-console.log(userPressed);
-
-if(userPressed === computerGuess){
-    winCount = ++winCount; 
-    document.getElementById("win-count").textContent = winCount;
-}
-else {
+    //compare computer pick + user pick 
+  
     guessLeft = -- guessLeft;
     document.getElementById("guess-left").textContent = guessLeft;
-}
+
+    if(userPressed === computerGuess){
+        winCount = ++winCount; 
+        document.getElementById("win-count").textContent = winCount;
+        guessNewLetter();
+        reset();
+    }else if(guessMade.length == 9){
+        loseCount = ++loseCount;
+        document.getElementById("lose-count").textContent = loseCount;
+        guessNewLetter();
+        reset();
+    }
 
 
 }
-
-// show user win/lose
-
-
-
-// show amount of guesses left
-
-
-// reset the win and loss count after guesscount reaches 0 (without refreshing the page)
