e9a05adc07c960f0da2e5ca2dd9dd03d8ceaed53
Adrienne <iliu.adrienne@gmail.com>
2019-05-23 19:24:24 -0700

    display player's letter guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 
 var winCount = 0; 
 var loseCount = 0; 
-var guessesLeft = 9; 
+var guessLeft = 9; 
 var guessMade = []; 
 var computerGuess = [];
 
@@ -10,24 +10,29 @@ var computerGuess = [];
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 
 
-// document.onkeyup() = function(event) {
 
-    var playerGuess = 
-    
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // make the computer guess a letter
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log(computerGuess);
 
 
-// show amount of guesses left
-
-
 // show user what they've guessed so far
 
+document.onkeyup = function(input){
+
+var userPressed = event.key.toLowerCase();
+
+guessMade.push(userPressed);
+document.getElementById("guess-made").textContent = guessMade;
+
+// show amount of guesses left
+}
 
 
-// }
+
+
+
+
 // declare a variable for the array we're storing the history of user key presses into and assign it to an empty array
 
 
